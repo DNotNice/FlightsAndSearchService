dd28d85004dfe818ad0833da689b480ce1f85599
Devansh Pandey <todevanshp@gmail.com>
2023-01-22 01:03:22 +0530

    added crud functionalities

--- src/controllers/airport-controller.js
+++ src/controllers/airport-controller.js
@@ -1,5 +1,5 @@
 const {AirportService} = require('../services/index');
- const airportService = new AirportService;
+ const airportService = new AirportService();
 const create = async (req ,res)=>{
     try {
         const response = await airportService.create(req.body);
